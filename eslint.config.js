88ff793e79f890254912b565f0887cafb0598c78
Ed Preston <epreston@prestonsoft.com>
2024-08-07 05:53:40 +1000

    Update eslint.config.js
    'FlatConfig' is deprecated.

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import sharedConfig from '@epreston/eslint-config';
 import pluginStorybook from 'eslint-plugin-storybook';
 import pluginVue from 'eslint-plugin-vue';
 
-/** @type { import('eslint').Linter.FlatConfig[] } */
+/** @type { import('eslint').Linter.Config[] } */
 export default [
   ...sharedConfig,
   ...pluginVue.configs['flat/essential'],
